aecfabb103ddd03941f6005ad3730b287146fdfa
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-28 21:52:35 +0700

    Validation Login Activity

--- JettyLoginApp/app/src/main/java/android/huyhuynh/jettyloginapp/LoginActivity.java
+++ JettyLoginApp/app/src/main/java/android/huyhuynh/jettyloginapp/LoginActivity.java
@@ -6,16 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     ImageButton imgRegister;
     EditText edtMail, edtPass;
+    final String email_pattern = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,31 @@ public class LoginActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        controlLogin();
+    }
+
+    private void controlLogin() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtMail.getText().toString().trim();
+                String pass = edtPass.getText().toString().trim();
+                if (email.length()==0 | pass.length()==0){
+                    Toast.makeText(LoginActivity.this,"Vui lòng điền đầy đủ thông tin đăng nhập"
+                    ,Toast.LENGTH_LONG).show();
+                } else if (!email.matches(email_pattern)){
+                    Toast.makeText(LoginActivity.this,"Bạn nhập sai định dạng email!",Toast.LENGTH_LONG).show();
+                } else if (pass.length()<8){
+                    Toast.makeText(LoginActivity.this,"Mật khẩu phải có ít nhất 8 kí tự!"
+                            ,Toast.LENGTH_LONG).show();
+                } else {
+                    //Trường hợp đăng nhập thành công
+                    Toast.makeText(LoginActivity.this,"OK!"
+                            ,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
